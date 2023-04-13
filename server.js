e518524f2972e0a60de6452d3c5886d64a0c6115
eAmartya <eageramartyacodingrn@gmail.com>
2023-04-13 21:53:55 +0530

    did basic clock

--- server.js
+++ server.js
@@ -8,11 +8,14 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 // app.set(`view engine`, `ejs`);
 // app.use(express.static('public'))
+// app.use(express.static('public/'))
 
 
 
-app.get('/',(req,res)={
 
+app.get("/",function(req,res){
+    app.use("/public", express.static(__dirname + "/public"));
+    res.sendFile(__dirname+"/index.html")
 })
 
 
